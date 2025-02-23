a92aae461e29142c3ba37f95be43180a5fb76b6a
Bartek <kierunb@gmail.com>
2025-02-23 21:25:01 +0100

    Refactor message handling and update worker delay
    - Removed the `Hello` record and replaced it with `HelloMessage` in the `Contracts` namespace.
    - Deleted the `HelloEnpoint` class and its GET mapping in `HelloEndpoint.cs`.
    - Increased the worker delay from 1 second to 30 seconds in `Worker.cs`.
    - Added `SendHelloMessageEndpoint` for publishing hello messages via a new POST endpoint.
    - Introduced `HelloMessageConsumer` to log received messages in the worker service.

---
+++ WebApiMessageQueues/Endpoints/SendHelloMessageEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using WebApiMessageQueues.Extensions;
+
+namespace WebApiMessageQueues.Endpoints;
+
+public record SendHelloMessageRequest(string Message);
+
+public class SendHelloMessageEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost(
+            "/send-hello",
+            async (
+                [FromServices] IPublishEndpoint publishEndpoint,
+                SendHelloMessageRequest request
+            ) =>
+            {
+                await publishEndpoint.Publish(new HelloMessage { Message = request.Message });
+
+                return Results.Accepted();
+            }
+        );
+    }
+}
---
+++ WorkerService/Consumers/HelloMessageConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace WorkerService.Consumers;
+
+public class HelloMessageConsumer : IConsumer<HelloMessage>
+{
+    private readonly ILogger<HelloMessageConsumer> _logger;
+
+    public HelloMessageConsumer(ILogger<HelloMessageConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<HelloMessage> context)
+    {
+        _logger.LogInformation("Message received. Message: {Message}", context.Message.Message);
+        return Task.CompletedTask;
+    }
+}
